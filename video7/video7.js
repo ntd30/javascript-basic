3baf86822f1c9b42c10f12de89c2d78c00129846
ntd30 <nguyentienduy14119@gmail.com>
2024-08-22 16:27:43 +0700

    chapter 2 video 8

--- video7/video7.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello world form HTML');
-
-let a = 5, b = 7;
-
-console.log(`a + b = ${a} + ${b} = ${a + b}
-haha /
-    /;,.[{]\}]`);
---
+++ video8/video8.js
@@ -0,0 +1,18 @@
+let obj = {
+    name: 'Duy',
+    address: 'Ha Noi'
+};
+
+let b = 'Duy';
+
+let c = 
+
+console.log(typeof obj, typeof b);
+
+console.log(`My name is ${obj['name']}
+and my address is ${obj.address}`)
+
+let x = 'name';
+obj[x] = 'Giang';
+
+console.log(obj.name);
