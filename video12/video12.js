7be490c1fe5e222320256b2fc1107e49d4733507
ntd30 <nguyentienduy14119@gmail.com>
2024-08-23 11:33:12 +0700

    chapter 3 video 12

---
+++ video12/video12.js
@@ -0,0 +1,13 @@
+console.log('Hello')
+
+let arrTop4 = ['Man City', 'Liverpool', 'MU', 'Chelsea'];
+
+// for (let i = 0; i < arrTop4.length; i++) {
+//     console.log(`Top ${i + 1}: ${arrTop4[i]}`);
+// }
+
+let i = 0;
+while (i < arrTop4.length) {
+    console.log(`Top ${i + 1}: ${arrTop4[i]}`);
+    i++;
+}
