9ce503536e86fd4c6c97108ecb91e2233f82195f
ntd30 <nguyentienduy14119@gmail.com>
2024-08-23 10:57:22 +0700

    chapter 2 video 10

---
+++ video10/video10.js
@@ -0,0 +1,18 @@
+console.log('Hello world')
+
+let a = 5;
+let b = '5';
+
+console.log(0 == '')
+
+let c = ''
+
+console.log(typeof c)
+
+c = 5
+
+console.log(typeof c)
+
+c = []
+
+console.log(typeof c)
